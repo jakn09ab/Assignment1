e944bf30a26e0b29783010b372c241fd4d812ba0
Jakob Knudsen <JakobKnudsen@jakobs-imac.home>
2017-02-28 18:36:14 +0100

    made a little more on analysis. Made the merge work. It creates a sex.x and a sex.y that have identical values though.

--- ch1_analysis.R
+++ ch1_analysis.R
@@ -6,10 +6,16 @@ source('ch1_simContinuous.R')
 source('ch1_simCategorical.R')
 
 # merge two data frames by ID
-total <- merge(dataContinuous,dataCategorical="id")
+total <- merge(dataContinuous,dataCategorical, by= "id")
 
 #change type to factor
 
+#make summary of continuous variables use tapply summary Calculate descriptive statistics overall (e.g. summary function) and by sex (summary within a tapply) using the summary/tapply and table/prop.table functions for continuous and categorical variables, respectively.
+#* Median (Q1-Q3) for continuous variables
+#* Frequency (%) for categorical variables
 summary(total)
+
+# table/prop.table functions forcategorical variables
+
 #summary by sex
 tapply(total, )

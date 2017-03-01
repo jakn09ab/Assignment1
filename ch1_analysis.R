a3e0919aca10ffb58554fed51bfa8b7da8960d6d
Omar Silverman <o.silverman.r@gmail.com>
2017-03-01 15:34:47 +0100

    An update version of the code

--- ch1_analysis.R
+++ ch1_analysis.R
@@ -10,26 +10,38 @@ total <- merge(dataContinuous,dataCategorical, by= "id")
 total["sex"] <- total$sex.x
 
 #   removes excess columns
-total$sex.x <- NULL 
+#total$sex.x <- NULL 
 total$sex.y <- NULL 
 
-#change type to factor
+#We could skip the prior step if we give the same name to the id and sex variables
+
+#change numeric storage type to factor
+
 total$ethnic <- factor (total$ethnic,
                  levels = c(0, 1),
                  labels = c("non-white", "white"))
-total$sex <- factor (total$sex,
+
+total$sex.x <- factor (total$sex.x,
                         levels = c(0, 1),
                         labels = c("male", "female"))
 
 #make summary of continuous variables use tapply summary Calculate descriptive statistics overall (e.g. summary function) and by sex (summary within a tapply) using the summary/tapply and table/prop.table functions for continuous and categorical variables, respectively.
+
 #* Median (Q1-Q3) for continuous variables
 #* Frequency (%) for categorical variables
+
 summary(total)
 
-#summary by sex
-tapply(total$age, total$sex, summary)
-tapply(total$bmi, total$sex, summary)
-tapply(total$ethnic, total$sex, table)
-tapply(total$smoke, total$sex, table)
+#Descriptive statistics stratified by sex
 
-tapply(total$age, total$sex, hist)
+tapply(total$age, total$sex.x, summary)
+
+tapply(total$bmi, total$sex.x, summary)
+
+prop.table(table(total$sex.x, total$ethnic), margin = 1)
+
+prop.table(table(total$sex.x, total$smoke), margin = 1)
+
+tapply(total$age, total$sex.x, hist)
+
+

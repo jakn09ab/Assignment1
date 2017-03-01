c042df19749916a9731f4cb7f2b9d048dbb00227
Omar Silverman <o.silverman.r@gmail.com>
2017-03-01 14:01:04 +0100

    Corrected version of step 1

--- ch1_simCategorical.R
+++ ch1_simCategorical.R
@@ -7,25 +7,21 @@ set.seed(1)
 id <-c(1:1000)
 
 #Generate sex
-sex <- c(rep.int(0,600))
-sex <- append (sex, rep.int(1, 400))
+sex <- c(rep.int(0, 600), (rep.int(1, 400)))
+
+#Output data frame
+dataCategorical = data.frame(sex, id)
 
 #Generate ethnicity
-ethnic <-rbinom(1000, 1, 0.95)
-
-#Need to find out how to change values in ethnicity
-
+ethnic <-rbinom(1000, 1, 0.05)
 
 #Generate smoking status
 smoke <- c(0, 1, 2)
 
-smoke <- ifelse(sample(smoke, 1000, replace = TRUE, prob = c(0.5, 0.3, 0.2)), "sex"==0, sample(smoke, 1000, replace = TRUE, prob = c(0.6, 0.3, 0.1)))
-
+smoke <- ifelse(dataCategorical$sex == 0, sample(smoke, 600, replace = TRUE, prob = c(0.5, 0.3, 0.2)), sample(smoke, 400, replace = TRUE, prob = c(0.6, 0.3, 0.1)))
 
 smoke <- factor (smoke,
                    levels = c(0, 1, 2),
                    labels = c("never", "ex", "current"))
 
-#Output data frame
-dataCategorical = data.frame(sex, id, ethnic, smoke)
 

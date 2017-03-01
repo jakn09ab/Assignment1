69b7ea5369890dcbc00564ad53f82898c54ddc83
Omar Silverman <o.silverman.r@gmail.com>
2017-03-01 14:53:28 +0100

    New version of data frame- step 1, I realized that past version only included id and sex

--- ch1_simCategorical.R
+++ ch1_simCategorical.R
@@ -13,15 +13,16 @@ sex <- c(rep.int(0, 600), (rep.int(1, 400)))
 dataCategorical = data.frame(sex, id)
 
 #Generate ethnicity
-ethnic <-rbinom(1000, 1, 0.05)
+dataCategorical$ethnic <-rbinom(1000, 1, 0.05)
 
 #Generate smoking status
 smoke <- c(0, 1, 2)
 
-smoke <- ifelse(dataCategorical$sex == 0, sample(smoke, 600, replace = TRUE, prob = c(0.5, 0.3, 0.2)), sample(smoke, 400, replace = TRUE, prob = c(0.6, 0.3, 0.1)))
+dataCategorical$smoke <- ifelse(dataCategorical$sex == 0, sample(smoke, 600, replace = TRUE, prob = c(0.5, 0.3, 0.2)), sample(smoke, 400, replace = TRUE, prob = c(0.6, 0.3, 0.1)))
 
-smoke <- factor (smoke,
+dataCategorical$smoke <- factor (dataCategorical$smoke,
                    levels = c(0, 1, 2),
                    labels = c("never", "ex", "current"))
+
 
 
